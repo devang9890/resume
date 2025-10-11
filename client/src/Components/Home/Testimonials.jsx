e1763af942fcaf937ddceb64528b8535609f215d
Your Name <you@example.com>
2025-10-11 23:18:25 +0530

    updated hero

---
+++ client/src/Components/Home/Testimonials.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Title from './Title'
+import { BookUserIcon, Zap } from 'lucide-react'
+
+const Testimonials = () => {
+  return (
+    <div>
+      <div id='testimonials' className='flex flex-col items-center my-10 scroll-mt-12'>
+        <div className="flex items-center gap-2 text-sm text-green-600 bg-green-400/10 rounded-full px-6 py-1.5">
+          <BookUserIcon className='size-4.5 stroke-green-600' />
+          <span>Testimonials</span>
+        </div>
+
+        <Title
+          title="Don't just take our words"
+          description="Hear what our users say about us. We're always looking for ways to improve. If you have a positive experience with us, leave a review."
+        />
+      </div>
+
+      <div>
+        {/* You can add testimonial cards here later */}
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Banner from '../Components/Home/Banner'
+import Hero from '../Components/Home/Hero'
+import Features from '../Components/Home/Features'
+import Testimonials from '../Components/Home/Testimonials'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Hero/>
+      <Features/>
+      <Testimonials/>
     </div>
   )
 }
