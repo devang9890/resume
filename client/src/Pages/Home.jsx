902cfbcdba6289c1f9148d460e50dbfdf2ea80ff
Your Name <you@example.com>
2025-10-12 21:44:13 +0530

    updated footer

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../Components/Home/Hero'
 import Features from '../Components/Home/Features'
 import Testimonials from '../Components/Home/Testimonials'
 import CallToAction from '../Components/Home/CallToAction'
+import Footer from '../Components/Home/Footer'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Features/>
       <Testimonials/>
       <CallToAction/>
+      <Footer/>
     </div>
   )
 }
