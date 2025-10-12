c10ab65c6222c7602f366f08f02556aa63752507
Your Name <you@example.com>
2025-10-12 20:39:09 +0530

    updated

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Components/Home/Banner'
 import Hero from '../Components/Home/Hero'
 import Features from '../Components/Home/Features'
 import Testimonials from '../Components/Home/Testimonials'
+import CallToAction from '../Components/Home/CallToAction'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero/>
       <Features/>
       <Testimonials/>
+      <CallToAction/>
     </div>
   )
 }
